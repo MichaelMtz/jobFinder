e5bd063c49b3dee8f13f33edda13f1ff54c1ebd6
Maykel Martinez <mmartine@thoughtworks.com>
2015-08-10 16:41:29 -0400

    Calling the web API from angular

--- public/app/app.js
+++ public/app/app.js
@@ -1,8 +1,10 @@
-angular.module('app', []);
+angular.module('app', ['ngResource']);
 
-angular.module('app').controller('testCtrl', function($scope){
+angular.module('app').controller('testCtrl', function($scope, $resource){
 	//$scope.test = 'Angular working!!';
 
-	$scope.jobs = [{title: 'Sales person', description: 'you will fight dragons'},
-					{title: 'Accountant', description: 'you will use the keyboard'}];
+	// $scope.jobs = [{title: 'Sales person', description: 'you will fight dragons'},
+	// 				{title: 'Accountant', description: 'you will use the keyboard'}];
+
+	$scope.jobs = $resource('/api/jobs').query();
 });
